73b36c4d9e231b8f20563be81ae7bb4b8b303e78
Romain <romain.herault@les-tilleuls.coop>
2023-10-30 15:15:05 +0100

    feat: add vignette post processing

--- src/Effects.jsx
+++ src/Effects.jsx
@@ -1,5 +1,4 @@
-import { N8AO } from "@react-three/postprocessing"
-import { DepthOfField, EffectComposer, Noise, Bloom, Vignette, ToneMapping } from "@react-three/postprocessing"
+import { EffectComposer, Bloom, Vignette, ToneMapping } from "@react-three/postprocessing"
 import { BlendFunction } from 'postprocessing'
 
 export function Effects() {
@@ -13,6 +12,12 @@ export function Effects() {
             maxLuminance={10.0} // maximum luminance
             averageLuminance={1.0} // average luminance
             adaptationRate={1.0} // luminance adaptation rate
+        />
+          <Vignette
+            offset={0.5} // vignette offset
+            darkness={0.5} // vignette darkness
+            eskil={false} // Eskil's vignette technique
+            blendFunction={BlendFunction.NORMAL} // blend mode
         />
     </EffectComposer>
 }
